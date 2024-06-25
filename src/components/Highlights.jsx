017e2aeab610a84473136077cb580fd5b1132c7a
Fidelia M <fidelialtm13@gmail.com>
2024-06-25 17:11:53 +0200

    hero section done

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Highlights = () => {
+  return (
+    <div className='text-white'>Highlights</div>
+  )
+}
+
+export default Highlights
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Highlights from "./Highlights";
+
+export default {Hero, Navbar, Highlights}
